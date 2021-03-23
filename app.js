999956aa9b3deed47136f0553ef3c7e47e6f1e2e
Guilherme Correa Zago <guilherme.zago@pampa.compasso>
2021-03-23 11:33:04 -0300

    rota de user, delete and other stuff

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ require('./models/post.model')
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
+app.use(require('./routes/user'))
 
 
 app.listen(PORT, ()=> {
--- routes/post.js
+++ routes/post.js
@@ -7,6 +7,7 @@ const requireLogin = require('../middleware/requireLogin')
 router.get('/allposts', requireLogin, (req, res) => {
     Post.find()
       .populate("postedBy", "_id name")
+      .populate("comments.postedBy", "_id name")
         .then(posts => {
             res.json({posts})
         })
@@ -70,6 +71,43 @@ router.put('/unlike', requireLogin, (req, res) => {
             return res.status(422).json({error: err})
         } else {
             res.json(result)
+        }
+    })
+})
+
+router.put('/comment', requireLogin, (req, res) => {
+    const comment = { text:req.body.text, postedBy: req.user._id }
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+        if(err){
+            return res.status(422).json({error: err})
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+router.delete('/delete/:postId', requireLogin, (req, res) => {
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy", "_id name")
+    .exec((err, post) => {
+        if(err || !post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+            post.remove()
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
         }
     })
 })
